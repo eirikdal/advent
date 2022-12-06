58a0e6e1c4e7a70c7fa450bda7718d94c4959bf2
Eirik Daleng Haukedal <eirik.haukedal@gmail.com>
2022-12-06 20:13:11 +0100

    day 3 second part

--- day3/day3.go
+++ day3/day3.go
@@ -7,6 +7,44 @@ import (
 func main() {
 	contents := files.ReadFile("input")
 
+	//part1(contents)
+	part2(contents)
+}
+
+func part2(contents []string) {
+	sum := 0
+
+	for i := 0; i < len(contents); i += 3 {
+		first := GetValueMap(contents[i])
+		snd := GetValueMap(contents[i+1])
+		thrd := GetValueMap(contents[i+2])
+
+		hits1 := make(map[int]int, 53)
+		for i := 0; i < len(first); i++ {
+			hits1[int(first[i])] += 1
+		}
+		hits2 := make(map[int]int, 53)
+		for i := 0; i < len(snd); i++ {
+			hits2[int(snd[i])] += 1
+		}
+
+		hits3 := make(map[int]int, 53)
+		for i := 0; i < len(thrd); i++ {
+			hits3[int(thrd[i])] += 1
+		}
+
+		for i := 0; i < 53; i++ {
+			if hits1[i] > 0 && hits2[i] > 0 && hits3[i] > 0 {
+				sum += i
+				break
+			}
+		}
+	}
+
+	println(sum)
+}
+
+func part1(contents []string) {
 	sum := 0
 	for _, content := range contents {
 		half := len(content) / 2
